25120b42b30d23ab4182ab821b0e5211928e4836
royal450 <159676323+royal450@users.noreply.github.com>
2025-02-10 15:54:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')  
 def home():  
-    return "Hello, Flask on Render!"  
+    return "Hello, Flask on Render , Welcome Babu Shuhana Ab Jao Bol Uss Jhola Chhap ko Tera Server Puri duniya me Live Hai Aasam Me Bhi"  
 
 if __name__ == '__main__':  
     app.run(debug=True)
