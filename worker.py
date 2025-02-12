f6cec2768b7e7ce6f591fe2e19be5bf7af1986be
royal450 <159676323+royal450@users.noreply.github.com>
2025-02-12 13:55:51 +0530

    Delete worker.py

--- worker.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-
-app = Celery("tasks", broker="redis://localhost:6379/0")
-
-@app.task
-def process_pdf_to_mp3(filename):
-    # Same conversion logic here
-    return "MP3 File Ready!"
